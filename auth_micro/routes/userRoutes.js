4606886e00243764bd558a6c9a2d19dbb1bdb05b
rajat <rajat.cse18@chitkarauniversity.edu.in>
2025-01-08 17:02:59 +0530

    Wrapped Auth Microservicies

--- auth_micro/routes/userRoutes.js
+++ auth_micro/routes/userRoutes.js
@@ -1,12 +1,10 @@
 import { Router } from "express";
 import AuthContoller from "../controller/AuthController.js";
 import authMiddleware from "../middleware/AuthMiddleware.js";
+import UserController from "../controller/UserController.js";
 
 const router = Router();
 
-router.post("/auth/register", AuthContoller.register);
-router.post("/auth/login", AuthContoller.login);
-
-router.get("/auth/user", authMiddleware, AuthContoller.user);
+router.get("/getUser/:id", UserController.getUser);
 
 export default router;
